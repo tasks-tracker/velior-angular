27127cac480f1509b68def1807304a627b5615a5
ethrealelement <vitalik.safonov@list.ru>
2026-01-10 17:42:22 +0300

    Implement user authentication flow with loading state and dashboard redirection. Refactor app configuration to include an auth interceptor and update routing to support dashboard navigation. Add user service for handling user data and loading state management.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,21 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { API_URL } from '@shared/config/api.config';
+import { authInterceptor } from '@shared/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(withEventReplay()),
-    provideHttpClient(withInterceptorsFromDi()),
+    provideHttpClient(withInterceptors([authInterceptor])),
     {
       provide: API_URL,
-      useValue: 'http://localhost:8000', // Можно заменить на environment переменную
+      useValue: 'http://localhost:8000',
     },
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,23 @@
 import { Routes } from '@angular/router';
 import { LoginPage } from '@pages/login/ui/login-page/login-page';
 import { SignInPage } from '@pages/login/ui/sign-in-page/sign-in-page';
+import { DashboardPage } from './pages/dashboard/ui/dashboard-page/dashboard-page';
 
 export const routes: Routes = [
   {
-    path: 'login',
-    component: LoginPage,
+    path: '**',
+    redirectTo: 'dashboard',
   },
   {
     path: 'sign-in',
     component: SignInPage,
+  },
+  {
+    path: 'dashboard',
+    component: DashboardPage,
+  },
+  {
+    path: 'login',
+    component: LoginPage,
   },
 ];
