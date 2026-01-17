5c149bd1c1e38533717a5eb1b790a7da62ebfdd2
ethrealelement <vitalik.safonov@list.ru>
2026-01-17 15:08:10 +0300

    Add chat and conversations components to dashboard layout
    - Implemented Conversations and Chat components with respective HTML, SCSS, and TypeScript files.
    - Updated Dashboard component to include Conversations and Chat components.
    - Created a Search component for conversation filtering.
    - Added a CutTextPipe for text truncation.
    - Removed outdated dashboard test file.
    - Cleaned up app.ts by removing unnecessary console logs.

---
+++ src/app/features/dashboard/ui/chat/chat.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-chat',
+  imports: [
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule
+  ],
+  templateUrl: './chat.html',
+  standalone: true,
+  styleUrl: './chat.scss',
+})
+export class Chat {
+}
--- src/app/features/dashboard/ui/dashboard/dashboard.ts
+++ src/app/features/dashboard/ui/dashboard/dashboard.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { Conversations } from '../conversations/conversations';
+import { Chat } from '../chat/chat';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [Conversations, Chat],
   templateUrl: './dashboard.html',
+  standalone: true,
   styleUrl: './dashboard.scss',
 })
 export class Dashboard {
-
 }
