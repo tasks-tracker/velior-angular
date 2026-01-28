d01a3c3768c16ed28b21d5f85a457cae8b399e50
ethrealelement <vitalik.safonov@list.ru>
2026-01-28 14:28:37 +0300

    Refactor app configuration and add auth interceptor
    - Updated app configuration to use the correct path for the authInterceptor.
    - Introduced a new authInterceptor to manage credentials for HTTP requests, ensuring session cookies are sent with each request.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,13 +4,13 @@ import {
   provideAppInitializer,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter, Router } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { API_URL } from '@shared/config/api.config';
-import { authInterceptor } from '@shared/interceptors/auth.interceptor';
+import { authInterceptor } from '@app/shared/lib/interceptors/auth.interceptor';
 import { UserService } from '@app/entities/user/model/user.service';
 
 export const appConfig: ApplicationConfig = {
